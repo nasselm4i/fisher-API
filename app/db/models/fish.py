3d659fc365ec69fd935fcca35b928bbd29877a40
Nassim Massaudi <naelm31@ulaval.ca>
2023-07-28 16:21:10 -0400

    update function file and psycopg2 trying

--- app/db/models/fish.py
+++ app/db/models/fish.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime, Enum, Text
+from sqlalchemy import Column, Integer, String,  ForeignKey
 from sqlalchemy.orm import relationship
 
 from ..db_setup import Base
--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime, Enum, Text, DefaultClause
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DefaultClause
 
 from ..db_setup import Base
 
